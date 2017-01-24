a2a7749193a44af350621c8fce41ba90dc709b15
Felipe Rojas <wfelipe3@gmail.com>
2017-01-24 08:58:56 -0500

    project setup

---
+++ discovery/build.sbt
@@ -0,0 +1,9 @@
+name := "discovery"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.12" % "3.2.0-SNAP1"
+
+
---
+++ discovery/src/test/scala-2.12/com/discovery/SetupTest.scala
@@ -0,0 +1,13 @@
+package com.discovery
+
+import org.scalatest.{FreeSpec, Matchers}
+
+/**
+  * Created by dev-williame on 1/24/17.
+  */
+class SetupTest extends FreeSpec with Matchers {
+
+  "This is the project setup test" - {
+    "setup" should be("setup")
+  }
+}
