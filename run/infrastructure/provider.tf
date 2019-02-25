055fe75f8a89493313d213a2bea11214c27a93bf
felipe rojas <feliperojas@pop-os.localdomain>
2019-02-25 18:29:22 -0500

    init modules

--- run/infrastructure/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "azurerm" {
-  subscription_id = "${var.credential["subscription_id"]}"
-  client_id = "${var.credential["client_id"]}"
-  client_secret = "${var.credential["client_secret"]}"
-  tenant_id = "${var.credential["tenant_id"]}"
-}
