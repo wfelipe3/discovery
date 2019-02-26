6a9306545bffde44c353659441c62aa38bf2b05d
felipe rojas <feliperojas@pop-os.localdomain>
2019-02-26 10:00:40 -0500

    publish command

--- run/infrastructure/run.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-  name = "${var.location}-${var.prefix_rs}-rg-${var.channel_g}"
-  location = "${var.location}"
-}
-
-terraform {
-  backend "azurerm" {}
-}
