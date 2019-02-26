1be6a9031bde9e18f69753bfca878916622d0228
felipe rojas <feliperojas@pop-os.localdomain>
2019-02-26 10:04:45 -0500

    'update'

--- run/infrastructure/run.tf
+++ run/infrastructure/run.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
   name = "${var.location}-${var.prefix_rs}-rg-${var.channel_g}"
-  location = "${var.location}"
+  location = "${var.locations}"
 }
 
 terraform {
